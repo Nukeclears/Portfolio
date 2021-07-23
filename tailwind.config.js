0faabcea07d7cbe187b9378270bf92aad63dea31
Thomas <thomasvdeursen@gmail.com>
2021-07-23 23:30:27 +0200

    Build fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-    purge: ['./dist/*.html'],
+    purge: ['./docs/*.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},

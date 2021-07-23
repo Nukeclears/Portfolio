730ca1f3e7f6c105976fbfbe17a7f4784342ddca
Thomas <thomasvdeursen@gmail.com>
2021-07-23 23:20:22 +0200

    Initial progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     purge: ['./dist/*.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
+        fontFamily: {
+            'display': 'Montserrat, sans-serif',
+            'body': 'Noto Sans, sans-serif',
+        },
+        colors: {
+            // Build your palette here
+            transparent: 'transparent',
+            current: 'currentColor',
+            gray: colors.trueGray,
+            red: colors.red,
+            blue: colors.cyan,
+            yellow: colors.amber,
+            white: colors.white,
+            black: colors.black,
+        }
     },
     variants: {
         extend: {},

5061fe0c3986f5be4d5b6e6aaef1b4a043f97c9b
Thomas <thomasvdeursen@gmail.com>
2021-07-24 03:33:55 +0200

    Dark mode, lots of new things!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './style.less'
+import './Components.less'
+
+  document.getElementById("darktoggle").addEventListener("click", function() {
+      if (document.getElementById('body').classList.contains('dark')) {
+          document.getElementById('body').classList.remove('dark');
+      } else {
+        document.getElementById('body').classList.add('dark');
+      }
+    
+  });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,53 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
     purge: ['./docs/*.html'],
-    darkMode: false, // or 'media' or 'class'
+    darkMode: 'class', // or 'media' or 'class'
     theme: {
         extend: {
             backgroundImage: theme => ({
-                'header-background': "url('/src/images/Head_background.png')",
-               })
+                'header-background': "url('/docs/images/Head_background.png')",
+            }),
+            colors: {
+                gray: {
+                    1000: '#0A0A0A'
+                },
+            },
+            keyframes: {
+                dropin: {
+                    '100%': { transform: 'scale(1)' },
+                    '0%': { transform: 'scale(1.1)' },
+                }
+            },
+            animation: {
+                dropin: 'dropin 1s ease-out',
+            },
         },
         fontFamily: {
             'display': 'Montserrat, sans-serif',
             'body': 'Noto Sans, sans-serif',
         },
+        
         colors: {
             // Build your palette here
             transparent: 'transparent',
             current: 'currentColor',
             gray: colors.trueGray,
             red: colors.red,
-            blue: colors.cyan,
+            amber: colors.amber,
+            rose: colors.rose,
+            blue: colors.blue,
+            cyan: colors.cyan,
             yellow: colors.amber,
+            pink: colors.pink,
+            orange: colors.orange,
             white: colors.white,
             black: colors.black,
         },
     },
     variants: {
-        extend: {},
+        extend: {
+            display: ['dark'],
+        },
     },
     plugins: [],
 }
